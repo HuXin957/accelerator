c8cc1af6b5089af7a446abe19fa0e8fb89398e66
huqiang <1095518902@qq.com>
2022-07-19 17:04:05 +0800

    rebase

--- app/components/index.js
+++ app/components/index.js
@@ -5,5 +5,6 @@ export {default as Button} from "app/components/button";
 export {default as List} from "app/components/base/List";
 export {default as Detail} from "app/components/base/Detail";
 export {default as ImagePreview} from "app/components/imagePreview";
+export {default as Slider} from "app/components/slider";
 
 
